0c7d1ade3ff70ffa9fcd32b41c837e947d00077f
jamesb-ais <James.B@accessitsoftware.com>
2023-09-14 16:50:58 +1200

    trying some stuff

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -6,7 +6,7 @@
 
 <script lang="ts">
     import { getDrawerStore } from '@skeletonlabs/skeleton';
-    
+    import { page } from '$app/stores'
     const drawerStore = getDrawerStore();
 
  function drawerClose(): void {
@@ -18,9 +18,11 @@
 
 <nav class="list-nav p-4">
     <ul>
-        <li><a href="/" on:click={drawerClose}>Notes</a></li>
-        <li><a href="/album" on:click={drawerClose}>Album</a></li>
-        <li><a href="/movies" on:click={drawerClose}>Movies</a></li>
+        <li><a href="/" aria-current={$page.url.pathname === '/'} on:click={drawerClose}>Notes</a></li>
+        <li><a href="/album" aria-current={$page.url.pathname === '/album'} on:click={drawerClose}>Album</a></li>
+        <li><a href="/movies" aria-current={$page.url.pathname === '/movies'} on:click={drawerClose}>Movies</a></li>
         <li><a href="/?test=3" on:click={drawerClose}>Other Page 3</a></li>
     </ul>
 </nav>
+
+<!-- aria-current={$page.url.pathname === '/'} usefull for accessibility and css if you want to know what page your on to  -->
